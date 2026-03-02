ec880288fe2b423d3a5c106a64ebdcb16a28852e
Prithivi <prithivi@users.noreply.github.com>
2026-03-02 17:33:21 +0545

    feat: set up initial backend structure with Express, middleware, and server configuration

---
+++ backend/src/server.ts
@@ -0,0 +1,24 @@
+import app from "./app.js";
+import connectToDatabase from "./config/db.config.js";
+import logger from "./lib/logger.lib.js";
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await connectToDatabase();
+    app.listen(PORT, () => {
+      logger.info(`Server is running on port ${PORT}`, {
+        label: "Server_Startup",
+      });
+    });
+  } catch (error) {
+    logger.error("Failed to start the server:", {
+      label: "Server_Startup",
+      error,
+    });
+    process.exit(1);
+  }
+};
+
+startServer();
