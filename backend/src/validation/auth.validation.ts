178724fd14328479af99a0a54fb6db5c991e6f32
Prithivi <prithivi@users.noreply.github.com>
2026-03-02 22:51:52 +0545

    feat: update user schema validation and add login user schema

--- backend/src/validation/auth.validation.ts
+++ backend/src/validation/auth.validation.ts
@@ -1,11 +1,17 @@
 import { z } from "zod";
 
 export const registerUserSchema = z.object({
-  name: z.string().min(2, "Name must be at least 2 characters long"),
+  name: z.string().min(6, "Name must be at least 6 characters long"),
   email: z.email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters long"),
   profileImageUrl: z.string().optional(),
   adminInviteToken: z.string().optional(),
 });
 
+export const loginUserSchema = z.object({
+  email: z.email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters long"),
+});
+
 export type RegisterUserInput = z.infer<typeof registerUserSchema>;
+export type LoginUserInput = z.infer<typeof loginUserSchema>;
