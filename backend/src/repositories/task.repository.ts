503927d5ce55a4433aae2982505da9a06f4c3859
Prithivi <prithivi@users.noreply.github.com>
2026-03-03 17:27:29 +0545

    feat: add task retrieval and counting functions in task repository

--- backend/src/repositories/task.repository.ts
+++ backend/src/repositories/task.repository.ts
@@ -6,3 +6,26 @@ type CreateTaskData = CreateTaskInput & { createdBy: string };
 export const createTask = async (taskData: CreateTaskData) => {
   return await Task.create(taskData);
 };
+
+export const findTasks = async (filter: any = {}) => {
+  return await Task.find(filter)
+    .populate("assignedTo", "name email profileImageUrl")
+    .lean();
+};
+
+export const allTaskCountDocument = async (role: Roles, userId: string) => {
+  return await Task.countDocuments(
+    role === "admin" ? {} : { assignedTo: userId },
+  ).lean();
+};
+
+export const statusTaskCountDocument = async (
+  filter: any = {},
+  role: Roles,
+  userId: string,
+) => {
+  return await Task.countDocuments({
+    ...filter,
+    ...(role !== "admin" && { assignedTo: userId }),
+  });
+};
