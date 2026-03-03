a1de03314d478d868fed584cdab7f60451770ef8
Prithivi <prithivi@users.noreply.github.com>
2026-03-03 12:06:56 +0545

    feat: implement getAllUser API route

--- backend/src/routes/index.route.ts
+++ backend/src/routes/index.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import authRouter from "./auth.route.js";
+import userRouter from "./user.route.js";
 
 const router: Router = Router();
 
@@ -12,6 +13,7 @@ router.route("/health").get((_req, res) => {
 });
 
 router.use("/api/auth", authRouter);
+router.use("/api/users", userRouter);
 
 router.use("/{*splat}", (_req, res) => {
   res.status(404).json({ error: "Route not found" });
